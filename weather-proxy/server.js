fab1ca966bbab305148372c1a12a309a7f588ffc
isaac-ca4sinho13 <162836887+isaac-ca4sinho13@users.noreply.github.com>
2025-05-29 10:54:17 -0300

    Update server.js

--- weather-proxy/server.js
+++ weather-proxy/server.js
@@ -7,7 +7,8 @@ app.use(cors());
 
 app.get('/weather', async (req, res) => {
   try {
-    const response = await axios.get('https://api.hgbrasil.com/weather?key=775fe3ed&woeid=455824');
+    const city = req.query.city;
+    const response = await axios.get(`https://api.hgbrasil.com/weather?key=775fe3ed&city_name=${city}`);
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: 'Erro ao buscar dados' });
